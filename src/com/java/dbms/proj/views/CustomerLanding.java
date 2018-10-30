ee26e1d255d379930005c3d02642e444271c1250
VishnuManthuruthil <vishnu18r@gmail.com>
2018-10-30 13:54:27 -0400

    Added customer landing view

---
+++ src/com/java/dbms/proj/views/CustomerLanding.java
@@ -0,0 +1,15 @@
+package com.java.dbms.proj.views;
+
+public class CustomerLanding {
+
+	public static void CustomerLanding() {
+		System.out.println("		Enter Your Choice :  ");
+		System.out.println("	1. Profile");
+		System.out.println("	2. Register Car");
+		System.out.println("	3. Services");
+		System.out.println("	4. Invoices");
+		System.out.println("	5. Logout");
+
+	}
+	
+}
