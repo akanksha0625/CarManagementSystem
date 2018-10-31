3958302a4fd3ce69b450b38ae8816ef776fbb458
carmenbentley <44073231+carmenbentley@users.noreply.github.com>
2018-10-31 15:30:43 -0400

    Added header

--- src/com/java/dbms/proj/views/ReceptionistLanding.java
+++ src/com/java/dbms/proj/views/ReceptionistLanding.java
@@ -3,6 +3,19 @@ package com.java.dbms.proj.views;
 public class ReceptionistLanding {
 
 	public static void displayPage() {
+		System.out.println("*************************************************************************************************************************************************************************");
+		System.out.println("| 888       888          888                                              8888888b.                                    888    d8b                   d8b          888    |");
+		System.out.println("| 888   o   888          888                                              888   Y88b                                   888    Y8P                   Y8P          888    |");
+		System.out.println("| 888  d8b  888          888                                              888    888                                   888                                       888    |");
+		System.out.println("| 888 d888b 888  .d88b.  888  .d8888b .d88b.  88888b.d88b.   .d88b.       888   d88P .d88b.   .d8888b .d88b.  88888b.  888888 888  .d88b.  88888b.  888 .d8888b  888888 |");
+		System.out.println("| 888d88888b888 d8P  Y8b 888 d88P\"   d88\"\"88b 888 \"888 \"88b d8P  Y8b      8888888P\" d8P  Y8b d88P\"   d8P  Y8b 888 \"88b 888    888 d88\"\"88b 888 \"88b 888 88K      888    |");
+		System.out.println("| 88888P Y88888 88888888 888 888     888  888 888  888  888 88888888      888 T88b  88888888 888     88888888 888  888 888    888 888  888 888  888 888 \"Y8888b. 888    |");
+		System.out.println("| 8888P   Y8888 Y8b.     888 Y88b.   Y88..88P 888  888  888 Y8b.          888  T88b Y8b.     Y88b.   Y8b.     888 d88P Y88b.  888 Y88..88P 888  888 888      X88 Y88b.  |");
+		System.out.println("| 888P     Y888  \"Y8888  888  \"Y8888P \"Y88P\"  888  888  888  \"Y8888       888   T88b \"Y8888   \"Y8888P \"Y8888  88888P\"   \"Y888 888  \"Y88P\"  888  888 888  88888P'  \"Y888 |");
+		System.out.println("|                                                                                                             888                                                       |");
+		System.out.println("|                                                                                                             888                                                       |");
+		System.out.println("|                                                                                                             888                                                       |");
+		System.out.println("*************************************************************************************************************************************************************************\n");
 		System.out.println("Please select from the following user options:");
 		System.out.println("\tEnter '1' to  Profile");
 		System.out.println("\tEnter '2' to	View Customer Profile");
