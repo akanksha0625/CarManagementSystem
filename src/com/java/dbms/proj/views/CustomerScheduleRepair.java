ff421fc0ed66fa065d2868d30f751509f0970b60
carmenbentley <44073231+carmenbentley@users.noreply.github.com>
2018-10-31 16:01:58 -0400

    New Views

---
+++ src/com/java/dbms/proj/views/CustomerScheduleRepair.java
@@ -0,0 +1,46 @@
+package com.java.dbms.proj.views;
+
+public class CustomerScheduleRepair {
+	public static void displayPageOne() {
+	System.out.println("******************************************************************************************************************************");
+	System.out.println("|  .d8888b.           888                    888          888               8888888b.                            d8b         |"); 
+	System.out.println("| d88P  Y88b          888                    888          888               888   Y88b                           Y8P         |");  
+	System.out.println("| Y88b.               888                    888          888               888    888                                       |");  
+	System.out.println("|  \"Y888b.    .d8888b 88888b.   .d88b.   .d88888 888  888 888  .d88b.       888   d88P .d88b.  88888b.   8888b.  888 888d888 |");  
+	System.out.println("|     \"Y88b. d88P\"    888 \"88b d8P  Y8b d88\" 888 888  888 888 d8P  Y8b      8888888P\" d8P  Y8b 888 \"88b     \"88b 888 888P\"   |"); 
+	System.out.println("|	    \"888 888      888  888 88888888 888  888 888  888 888 88888888      888 T88b  88888888 888  888 .d888888 888 888     |");  
+	System.out.println("| Y88b  d88P Y88b.    888  888 Y8b.     Y88b 888 Y88b 888 888 Y8b.          888  T88b Y8b.     888 d88P 888  888 888 888     |"); 
+	System.out.println("|  \"Y8888P\"   \"Y8888P 888  888  \"Y8888   \"Y88888  \"Y88888 888  \"Y8888       888   T88b \"Y8888  88888P\"  \"Y888888 888 888     |"); 
+	System.out.println("|	                                                                                           888                           |");  
+	System.out.println("|	                                                                                           888                           |"); 
+	System.out.println("|	                                                                                           888                           |"); 
+	System.out.println("******************************************************************************************************************************\n");
+		System.out.println("Please select from the following user options:");
+		System.out.println("\tEnter '1' to select \"Engine knock\"");
+		System.out.println("\tEnter '2' to select \"Car drifts in a particular direction\"");
+		System.out.println("\tEnter '3' to select \"Battery does not hold charge\"");
+		System.out.println("\tEnter '4' to select \"Black/unclean exhaust\"");
+		System.out.println("\tEnter '5' to select \"A/C or Heater not working\"");
+		System.out.println("\tEnter '6' to select \"Head-lamps or Tail-lamps not working\"");
+		System.out.println("\tEnter '7' to select \"Check engine light\"");
+		System.out.println("\tEnter '8' to Go Back");
+	}
+	public static void displayPageTwo() {
+	System.out.println("******************************************************************************************************************************");
+	System.out.println("|  .d8888b.           888                    888          888               8888888b.                            d8b         |"); 
+	System.out.println("| d88P  Y88b          888                    888          888               888   Y88b                           Y8P         |");  
+	System.out.println("| Y88b.               888                    888          888               888    888                                       |");  
+	System.out.println("|  \"Y888b.    .d8888b 88888b.   .d88b.   .d88888 888  888 888  .d88b.       888   d88P .d88b.  88888b.   8888b.  888 888d888 |");  
+	System.out.println("|     \"Y88b. d88P\"    888 \"88b d8P  Y8b d88\" 888 888  888 888 d8P  Y8b      8888888P\" d8P  Y8b 888 \"88b     \"88b 888 888P\"   |"); 
+	System.out.println("|	    \"888 888      888  888 88888888 888  888 888  888 888 88888888      888 T88b  88888888 888  888 .d888888 888 888     |");  
+	System.out.println("| Y88b  d88P Y88b.    888  888 Y8b.     Y88b 888 Y88b 888 888 Y8b.          888  T88b Y8b.     888 d88P 888  888 888 888     |"); 
+	System.out.println("|  \"Y8888P\"   \"Y8888P 888  888  \"Y8888   \"Y88888  \"Y88888 888  \"Y8888       888   T88b \"Y8888  88888P\"  \"Y888888 888 888     |"); 
+	System.out.println("|	                                                                                           888                           |");  
+	System.out.println("|	                                                                                           888                           |"); 
+	System.out.println("|	                                                                                           888                           |"); 
+	System.out.println("******************************************************************************************************************************\n");
+		System.out.println("Please select from the following user options:");
+		System.out.println("\tEnter '1' to schedule your date for Repair");
+		System.out.println("\tEnter '2' Go Back");
+	}
+}
