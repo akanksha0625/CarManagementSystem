73d0d9ac7e5203252b968574e968b0aa73351f0e
carmenbentley <44073231+carmenbentley@users.noreply.github.com>
2018-10-31 15:46:48 -0400

    Added Headers

--- src/com/java/dbms/proj/views/ManagerOrdersView.java
+++ src/com/java/dbms/proj/views/ManagerOrdersView.java
@@ -3,21 +3,54 @@ package com.java.dbms.proj.views;
 public class ManagerOrdersView {
 
 	public static void displayOrdersMenu() {
+		System.out.println("*****************************************************************************************************************************");
+		System.out.println("|                                    .d88888b.              888                                                             |");
+		System.out.println("|                                   d88P" "Y88b             888                                                             |");
+		System.out.println("|                                   888     888             888                                                             |");
+		System.out.println("|                                   888     888 888d888 .d88888  .d88b.  888d888 .d8888b                                    |");
+		System.out.println("|                                   888     888 888P\"  d88\" 888 d8P  Y8b 888P\"   88K                                        |");
+		System.out.println("|                                   888     888 888    888  888 88888888 888     \"Y8888b.                                   |");
+		System.out.println("|                                   Y88b. .d88P 888    Y88b 888 Y8b.     888          X88                                   |");
+		System.out.println("|                                    \"Y88888P\"  888     \"Y88888  \"Y8888  888      88888P'                                   |");
+		System.out.println("*****************************************************************************************************************************\n");
 		System.out.println("Please select from the following user options:");
-		System.out.println("\tEnter '1' to	Order History");
-		System.out.println("\tEnter '2' to	New Order");
-		System.out.println("\tEnter '3' to	Go Back");
+		System.out.println("\tEnter '1' to Order History");
+		System.out.println("\tEnter '2' to New Order");
+		System.out.println("\tEnter '3' to Go Back");
 	}
 	
 	public static void displayOrdersHistory() {
+		System.out.println("***************************************************************************************************************************");
+		System.out.println("|        .d88888b.              888                       888    888 d8b          888                                     |");
+		System.out.println("|       d88P\" \"Y88b             888                       888    888 Y8P          888                                     |");
+		System.out.println("|       888     888             888                       888    888              888                                     |");
+		System.out.println("|       888     888 888d888 .d88888  .d88b.  888d888      8888888888 888 .d8888b  888888 .d88b.  888d888 888  888         |");
+		System.out.println("|       888     888 888P\"  d88\" 888 d8P  Y8b 888P\"        888    888 888 88K      888   d88\"\"88b 888P\"   888  888         |");
+		System.out.println("|       888     888 888    888  888 88888888 888          888    888 888 \"Y8888b. 888   888  888 888     888  888         |");
+		System.out.println("|       Y88b. .d88P 888    Y88b 888 Y8b.     888          888    888 888      X88 Y88b. Y88..88P 888     Y88b 888         |");
+		System.out.println("|        \"Y88888P\"  888     \"Y88888  \"Y8888  888          888    888 888  88888P'  \"Y888 \"Y88P\"  888      \"Y88888         |");
+		System.out.println("|                                                                                                             888         |");
+		System.out.println("|                                                                                                        Y8b d88P         |");
+		System.out.println("|                                                                                                         \"Y88P\"          |");
+		System.out.println("***************************************************************************************************************************\n");
 		System.out.println("Please select from the following user options:");
-		System.out.println("\tEnter '1' to	Go Back");
+		System.out.println("\tEnter '1' to Go Back");
 	}
 	
 	public static void displayNewOrder() {
+		System.out.println("***************************************************************************************************************");
+		System.out.println("|             888b    888                              .d88888b.              888                              |");
+		System.out.println("|             8888b   888                             d88P\" \"Y88b             888                              |");
+		System.out.println("|             88888b  888                             888     888             888                              |");
+		System.out.println("|             888Y88b 888  .d88b.  888  888  888      888     888 888d888 .d88888  .d88b.  888d888             |");
+		System.out.println("|             888 Y88b888 d8P  Y8b 888  888  888      888     888 888P\"  d88\" 888 d8P  Y8b 888P\"               |");
+		System.out.println("|             888  Y88888 88888888 888  888  888      888     888 888    888  888 88888888 888                 |");
+		System.out.println("|             888   Y8888 Y8b.     Y88b 888 d88P      Y88b. .d88P 888    Y88b 888 Y8b.     888                 |");
+		System.out.println("|             888    Y888  \"Y8888   \"Y8888888P\"        \"Y88888P\"  888     \"Y88888  \"Y8888  888                 |");
+		System.out.println("****************************************************************************************************************\n");
 		System.out.println("Please select from the following user options:");
-		System.out.println("\tEnter '2' to	Place Orders");
-		System.out.println("\tEnter '1' to	Go Back");
+		System.out.println("\tEnter '2' to Place Orders");
+		System.out.println("\tEnter '1' to Go Back");
 
 	}
 }
