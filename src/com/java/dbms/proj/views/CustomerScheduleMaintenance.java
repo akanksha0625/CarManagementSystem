3654cf7a5e6f85d99d7673afabbf98e954d58314
carmenbentley <44073231+carmenbentley@users.noreply.github.com>
2018-10-31 15:58:40 -0400

    Corrected file

---
+++ src/com/java/dbms/proj/views/CustomerScheduleMaintenance.java
@@ -0,0 +1,36 @@
+package com.java.dbms.proj.views;
+
+public class CustomerScheduleMaintenance {
+	public static void displayPageOne() {
+	System.out.println("*****************************************************************************************************************************************************************************");
+	System.out.println("|   .d8888b.           888                    888          888               888b     d888          d8b          888                                                        |");
+	System.out.println("|  d88P  Y88b          888                    888          888               8888b   d8888          Y8P          888                                                        |");
+	System.out.println("|  Y88b.               888                    888          888               88888b.d88888                       888                                                        |");
+	System.out.println("|   \"Y888b.    .d8888b 88888b.   .d88b.   .d88888 888  888 888  .d88b.       888Y88888P888  8888b.  888 88888b.  888888 .d88b.  88888b.   8888b.  88888b.   .d8888b .d88b.  |");
+	System.out.println("|      \"Y88b. d88P\"    888 \"88b d8P  Y8b d88\" 888 888  888 888 d8P  Y8b      888 Y888P 888     \"88b 888 888 \"88b 888   d8P  Y8b 888 \"88b     \"88b 888 \"88b d88P\"   d8P  Y8b |");
+	System.out.println("|         888 888      888  888 88888888 888  888 888  888 888 88888888      888  Y8P  888 .d888888 888 888  888 888   88888888 888  888 .d888888 888  888 888     88888888 |");
+	System.out.println("|  Y88b  d88P Y88b.    888  888 Y8b.     Y88b 888 Y88b 888 888 Y8b.          888   \"   888 888  888 888 888  888 Y88b. Y8b.     888  888 888  888 888  888 Y88b.   Y8b.     |");
+	System.out.println("|   \"Y8888P\"   \"Y8888P 888  888  \"Y8888   \"Y88888  \"Y88888 888  \"Y8888       888       888 \"Y888888 888 888  888  \"Y888 \"Y8888  888  888 \"Y888888 888  888  \"Y8888P \"Y8888  |");
+	System.out.println("*****************************************************************************************************************************************************************************\n");
+		System.out.println("Please select from the following user options:");
+		System.out.println("\tEnter '1' to  Schedule Maintenance");
+		System.out.println("\tEnter '2' to	Schedule Repair");
+		System.out.println("\tEnter '3' to	Go Back");
+	}
+	
+	public static void displayPageTwo() {
+	System.out.println("*****************************************************************************************************************************************************************************");
+	System.out.println("|   .d8888b.           888                    888          888               888b     d888          d8b          888                                                        |");
+	System.out.println("|  d88P  Y88b          888                    888          888               8888b   d8888          Y8P          888                                                        |");
+	System.out.println("|  Y88b.               888                    888          888               88888b.d88888                       888                                                        |");
+	System.out.println("|   \"Y888b.    .d8888b 88888b.   .d88b.   .d88888 888  888 888  .d88b.       888Y88888P888  8888b.  888 88888b.  888888 .d88b.  88888b.   8888b.  88888b.   .d8888b .d88b.  |");
+	System.out.println("|      \"Y88b. d88P\"    888 \"88b d8P  Y8b d88\" 888 888  888 888 d8P  Y8b      888 Y888P 888     \"88b 888 888 \"88b 888   d8P  Y8b 888 \"88b     \"88b 888 \"88b d88P\"   d8P  Y8b |");
+	System.out.println("|         888 888      888  888 88888888 888  888 888  888 888 88888888      888  Y8P  888 .d888888 888 888  888 888   88888888 888  888 .d888888 888  888 888     88888888 |");
+	System.out.println("|  Y88b  d88P Y88b.    888  888 Y8b.     Y88b 888 Y88b 888 888 Y8b.          888   \"   888 888  888 888 888  888 Y88b. Y8b.     888  888 888  888 888  888 Y88b.   Y8b.     |");
+	System.out.println("|   \"Y8888P\"   \"Y8888P 888  888  \"Y8888   \"Y88888  \"Y88888 888  \"Y8888       888       888 \"Y888888 888 888  888  \"Y888 \"Y8888  888  888 \"Y888888 888  888  \"Y8888P \"Y8888  |");
+	System.out.println("*****************************************************************************************************************************************************************************\n");
+		System.out.println("Please select from the following user options:");
+		System.out.println("\tEnter '1' to  Schedule on Date");
+		System.out.println("\tEnter '2' to	Go Back");
+	}
+}
