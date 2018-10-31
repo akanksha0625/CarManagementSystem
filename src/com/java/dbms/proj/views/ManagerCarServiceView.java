29ae90a04a7b7289d7526e816ddb64cd07a00f46
carmenbentley <44073231+carmenbentley@users.noreply.github.com>
2018-10-31 15:52:49 -0400

    Added header

--- src/com/java/dbms/proj/views/ManagerCarServiceView.java
+++ src/com/java/dbms/proj/views/ManagerCarServiceView.java
@@ -3,6 +3,16 @@ package com.java.dbms.proj.views;
 public class ManagerCarServiceView {
 
 	public static void displayCarServiceDetails() {
+		System.out.println("********************************************************************************************************************************************************");
+		System.out.println("|  .d8888b.                         .d8888b.                            d8b                       8888888b.           888             d8b 888          |");
+		System.out.println("| d88P  Y88b                       d88P  Y88b                           Y8P                       888  \"Y88b          888             Y8P 888          |");
+		System.out.println("| 888    888                       Y88b.                                                          888    888          888                 888          |");
+		System.out.println("| 888         8888b.  888d888       \"Y888b.    .d88b.  888d888 888  888 888  .d8888b .d88b.       888    888  .d88b.  888888  8888b.  888 888 .d8888b  |");
+		System.out.println("| 888            \"88b 888P\"            \"Y88b. d8P  Y8b 888P\"   888  888 888 d88P\"   d8P  Y8b      888    888 d8P  Y8b 888        \"88b 888 888 88K      |");
+		System.out.println("| 888    888 .d888888 888                \"888 88888888 888     Y88  88P 888 888     88888888      888    888 88888888 888    .d888888 888 888 \"Y8888b. |");
+		System.out.println("| Y88b  d88P 888  888 888          Y88b  d88P Y8b.     888      Y8bd8P  888 Y88b.   Y8b.          888  .d88P Y8b.     Y88b.  888  888 888 888      X88 |");
+		System.out.println("|  \"Y8888P\"  \"Y888888 888           \"Y8888P\"   \"Y8888  888       Y88P   888  \"Y8888P \"Y8888       8888888P\"   \"Y8888   \"Y888 \"Y888888 888 888  88888P' |");
+		System.out.println("********************************************************************************************************************************************************\n");
 		System.out.println("Please select from the following user options:");
 		System.out.println("\tEnter '1' to	Go Back");
 	}
