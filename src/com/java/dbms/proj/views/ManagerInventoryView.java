86b527b41f2d58ce129cedf7f57450ffc3e79bd4
carmenbentley <44073231+carmenbentley@users.noreply.github.com>
2018-10-31 15:41:23 -0400

    Added Header

--- src/com/java/dbms/proj/views/ManagerInventoryView.java
+++ src/com/java/dbms/proj/views/ManagerInventoryView.java
@@ -3,7 +3,21 @@ package com.java.dbms.proj.views;
 public class ManagerInventoryView {
 
 	public static void displayView() {
+		System.out.println("*****************************************************************************************************************************");
+		System.out.println("|                     8888888                                     888                                                       |");
+		System.out.println("|                       888                                       888                                                       |");
+		System.out.println("|                       888                                       888                                                       |");
+		System.out.println("|                       888   88888b.  888  888  .d88b.  88888b.  888888 .d88b.  888d888 888  888                           |");
+		System.out.println("|                       888   888 \"88b 888  888 d8P  Y8b 888 \"88b 888   d88\"\"88b 888P\"   888  888                           |");
+		System.out.println("|                       888   888  888 Y88  88P 88888888 888  888 888   888  888 888     888  888                           |");
+		System.out.println("|                       888   888  888  Y8bd8P  Y8b.     888  888 Y88b. Y88..88P 888     Y88b 888                           |");
+		System.out.println("|                     8888888 888  888   Y88P    \"Y8888  888  888  \"Y888 \"Y88P\"  888      \"Y88888                           |");
+		System.out.println("|                                                                                             888                           |");
+		System.out.println("|                                                                                        Y8b d88P                           |");
+		System.out.println("|                                                                                         \"Y88P\"                            |");
+		System.out.println("*****************************************************************************************************************************\n");
+
 		System.out.println("Please select from the following user options:");
-		System.out.println("\tEnter '1' to	Go Back");
+		System.out.println("\tEnter '1' to Go Back");
 	}
 }
