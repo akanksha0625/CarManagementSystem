3f894a1a04ad37a133e13652197c92ec986b5515
carmenbentley <44073231+carmenbentley@users.noreply.github.com>
2018-10-31 15:49:02 -0400

    Added Headers

--- src/com/java/dbms/proj/views/ManagerNotificationsView.java
+++ src/com/java/dbms/proj/views/ManagerNotificationsView.java
@@ -1,13 +1,33 @@
 package com.java.dbms.proj.views;
 
 public class ManagerNotificationsView {
-	public static void displayNotifications() {
+	public static void displayNotifications() {		
+		System.out.println("***************************************************************************************************************************");
+		System.out.println("|        888b    888          888    d8b  .d888 d8b                   888    d8b                                          |");
+		System.out.println("|        8888b   888          888    Y8P d88P\"  Y8P                   888    Y8P                                          |");
+		System.out.println("|        88888b  888          888        888                          888                                                 |");
+		System.out.println("|        888Y88b 888  .d88b.  888888 888 888888 888  .d8888b  8888b.  888888 888  .d88b.  88888b.  .d8888b                |");
+		System.out.println("|        888 Y88b888 d88\"\"88b 888    888 888    888 d88P\"        \"88b 888    888 d88\"\"88b 888 \"88b 88K                    |");
+		System.out.println("|        888  Y88888 888  888 888    888 888    888 888      .d888888 888    888 888  888 888  888 \"Y8888b.               |");
+		System.out.println("|        888   Y8888 Y88..88P Y88b.  888 888    888 Y88b.    888  888 Y88b.  888 Y88..88P 888  888      X88               |");
+		System.out.println("|        888    Y888  \"Y88P\"   \"Y888 888 888    888  \"Y8888P \"Y888888  \"Y888 888  \"Y88P\"  888  888  88888P'               |");
+		System.out.println("***************************************************************************************************************************\n");
 		System.out.println("Please select from the following user options:");
 		System.out.println("\tEnter '1' to	Order ID");
 		System.out.println("\tEnter '2' to	Go Back");
 	}
 	
-	public static void displayNotificationsDetail() {
+	public static void displayNotificationsDetail() {		
+		System.out.println("*******************************************************************************************************************************************************");
+		System.out.println("| 888b    888          888    d8b  .d888 d8b                   888    d8b                        8888888b.           888             d8b 888          |");
+		System.out.println("| 8888b   888          888    Y8P d88P\"  Y8P                   888    Y8P                        888  \"Y88b          888             Y8P 888          |");
+		System.out.println("| 88888b  888          888        888                          888                               888    888          888                 888          |");
+		System.out.println("| 888Y88b 888  .d88b.  888888 888 888888 888  .d8888b  8888b.  888888 888  .d88b.  88888b.       888    888  .d88b.  888888  8888b.  888 888 .d8888b  |");
+		System.out.println("| 888 Y88b888 d88""88b 888    888 888    888 d88P"        "88b 888    888 d88""88b 888 \"88b      888    888 d8P  Y8b 888        \"88b 888 888 88K      |");
+		System.out.println("| 888  Y88888 888  888 888    888 888    888 888      .d888888 888    888 888  888 888  888      888    888 88888888 888    .d888888 888 888 \"Y8888b. |");
+		System.out.println("| 888   Y8888 Y88..88P Y88b.  888 888    888 Y88b.    888  888 Y88b.  888 Y88..88P 888  888      888  .d88P Y8b.     Y88b.  888  888 888 888      X88 |");
+		System.out.println("| 888    Y888  \"Y88P\"   \"Y888 888 888    888  \"Y8888P \"Y888888  \"Y888 888  \"Y88P\"  888  888      8888888P\"   \"Y8888   \"Y888 \"Y888888 888 888  88888P' |");
+		System.out.println("*******************************************************************************************************************************************************\n");
 		System.out.println("Please select from the following user options:");
 		System.out.println("\tEnter '1' to	Go Back");
 	}
