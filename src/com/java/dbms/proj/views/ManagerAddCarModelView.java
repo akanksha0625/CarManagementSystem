e9119b4725d2564865adf6d1266fa9be824f25dd
carmenbentley <44073231+carmenbentley@users.noreply.github.com>
2018-10-31 15:50:34 -0400

    Added header

--- src/com/java/dbms/proj/views/ManagerAddCarModelView.java
+++ src/com/java/dbms/proj/views/ManagerAddCarModelView.java
@@ -2,8 +2,19 @@ package com.java.dbms.proj.views;
 
 public class ManagerAddCarModelView {
 	public static void displayPage() {
+		System.out.println("*************************************************************************************************************************");
+		System.out.println("| 888b    888                              .d8888b.                        888b     d888               888          888 |");
+		System.out.println("| 8888b   888                             d88P  Y88b                       8888b   d8888               888          888 |");
+		System.out.println("| 88888b  888                             888    888                       88888b.d88888               888          888 |");
+		System.out.println("| 888Y88b 888  .d88b.  888  888  888      888         8888b.  888d888      888Y88888P888  .d88b.   .d88888  .d88b.  888 |");
+		System.out.println("| 888 Y88b888 d8P  Y8b 888  888  888      888            \"88b 888P\"        888 Y888P 888 d88\"\"88b d88\" 888 d8P  Y8b 888 |");
+		System.out.println("| 888  Y88888 88888888 888  888  888      888    888 .d888888 888          888  Y8P  888 888  888 888  888 88888888 888 |");
+		System.out.println("| 888   Y8888 Y8b.     Y88b 888 d88P      Y88b  d88P 888  888 888          888   \"   888 Y88..88P Y88b 888 Y8b.     888 |");
+		System.out.println("| 888    Y888  \"Y8888   \"Y8888888P\"        \"Y8888P\"  \"Y888888 888          888       888  \"Y88P\"   \"Y88888  \"Y8888  888 |");
+		System.out.println("*************************************************************************************************************************\n");
+
 		System.out.println("Please select from the following user options:");
-		System.out.println("\tEnter '1' to	Add Car");
-		System.out.println("\tEnter '2' to	Go Back");
+		System.out.println("\tEnter '1' to Add Car");
+		System.out.println("\tEnter '2' to Go Back");
 	}
 }
