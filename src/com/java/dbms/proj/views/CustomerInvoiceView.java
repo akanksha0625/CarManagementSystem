4d20329e0a1950fdc84441d37e334dd8a6b4e7f0
VishnuManthuruthil <vishnu18r@gmail.com>
2018-10-31 12:59:20 -0400

    new customer views

---
+++ src/com/java/dbms/proj/views/CustomerInvoiceView.java
@@ -0,0 +1,14 @@
+package com.java.dbms.proj.views;
+
+public class CustomerInvoiceView {
+	public static void displayPage() {
+		System.out.println("Please select from the following user options:");
+		System.out.println("\tEnter '1' to  View Invoice details");
+		System.out.println("\tEnter '2' to	Go back");
+	}
+	
+	public static void displayInvoiceDetailsPage() {
+		System.out.println("Please select from the following user options:");
+		System.out.println("\tEnter '1' to	Go back");
+	}
+}
---
+++ src/com/java/dbms/proj/views/CustomerRescheduleService.java
@@ -0,0 +1,14 @@
+package com.java.dbms.proj.views;
+
+public class CustomerRescheduleService {
+	public static void displayPage1() {
+			System.out.println("Please select from the following user options:");
+			System.out.println("\tEnter '1' to  Pick a service");
+			System.out.println("\tEnter '2' to	Go back");
+		}
+	public static void displayPage2() {
+		System.out.println("Please select from the following user options:");
+		System.out.println("\tEnter '1' to  Reschedule date");
+		System.out.println("\tEnter '2' to	Go back");
+	}
+}
